1a67b69946a9958cb6ecad933255865fa7895e5e
Билолидин Алимжанов <bilol586746@mail.ru>
2021-03-12 23:35:09 +0300

    ff

--- Homework/Source.cpp
+++ Homework/Source.cpp
@@ -1,29 +1,66 @@
 #include <iostream> 
-#include <cmath> 
 
 using namespace std;
 
-class Tre {
-public:
-	int a, b, c;
-	int Square() {
-		int p = Perimetr()/2;
-		int s = sqrt(p * (p - a) * (p - b) * (p - c));
-		return s;
-	}
-	int Perimetr() {
-		int P = a + b + c;
-		return P;
-	}
-};
+void fillArr(int arr[],int N);
+void showArr(int arr[], int N);
+void delOthermethod(int arr[], int N,int key);
+
+
 
 int main()
 {
-	Tre x; 
-	cin >> x.a;
-	cin >> x.b;
-	cin >> x.c;
-	cout << x.Perimetr()<<endl;
-	cout << x.Square();
+	setlocale(0, "Rus");
+    const int N=10;
+    int arr[N];
+    
+    cout << "массив: ";
+    fillArr(arr, N);
+    showArr(arr, N);
+    cout << endl;
+
+    int key;
+    cout << "¬ведите ключ: ";
+    cin >> key;
+
+    delOthermethod(arr, N, key);
+
+    cout << "массив: ";
+    showArr(arr,N);
+
 	return(0);
 }
+
+void delOthermethod(int arr[],int N, int key) {
+    int j = 1;
+    for (int i=1; i <= N; i++)
+    {
+        arr[j] = arr[i];
+        if (arr[i] !=  key) {
+            j++;
+        }
+        else {
+            break;
+        }
+    }
+      N = j;
+}
+
+
+void fillArr(int arr[], int N) {
+
+    for (int k = 1; k <= N; k++)
+    {
+        arr[k] = rand () % 10;
+    }
+}
+
+
+void showArr(int arr[], int N) {
+
+    for (int m = 1; m <= N; m++)
+    {
+        cout << arr[m] << " ";
+    }
+}
+
